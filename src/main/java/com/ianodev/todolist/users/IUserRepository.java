daa5274edc5419dbf0a5baf05d6813e12919f871
Iano Maciel <ianomaciel6385@gmail.com>
2023-10-13 01:20:31 -0400

    feat: criação do arquivo de repositório

---
+++ src/main/java/com/ianodev/todolist/users/IUserRepository.java
@@ -0,0 +1,9 @@
+package com.ianodev.todolist.users;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+
+}
