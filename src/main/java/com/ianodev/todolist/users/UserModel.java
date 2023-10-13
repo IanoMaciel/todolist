d02e2e00f1f033898f1e0271cddc01bdc202be31
Iano Maciel <ianomaciel6385@gmail.com>
2023-10-13 00:55:52 -0400

    feat: criação do user model

---
+++ src/main/java/com/ianodev/todolist/users/UserModel.java
@@ -0,0 +1,10 @@
+package com.ianodev.todolist.users;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+}
